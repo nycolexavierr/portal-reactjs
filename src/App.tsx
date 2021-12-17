82cb093788482b435468c86c005b16efb16dd096
Nycole Xavier <nycole.xavier@hotmail.com>
2021-12-16 22:50:00 -0400

    Lesson: Creating the button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Yo</h1>
+      <Navbar/>
     </div>
   );
 }
